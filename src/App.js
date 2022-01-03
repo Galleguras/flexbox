155e3b891bda4c2b7f00c75d033a3d3e7c376a56
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-01-03 13:41:29 +0100

    7 Ejemplo de propiedades en el contenedor Flex

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import './estilos.css';
 function App() {
   return (
     <div className="container">
-      <div>PRIMERO</div>
-      <div>SEGUNDO</div>
-      <div>TERCERO</div>
+      <header>
+        <div>
+          <p>Primera frase</p>
+        </div>
+        <div>
+          <p>Segunda frase</p>
+        </div>
+        <div>
+          <p>Tercera frase</p>
+        </div>
+        <img src="./imagen.jpg" />
+      </header>
+      <main>
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+        <div>E</div>
+        <div>F</div>
+        <div>G</div>
+      </main>
     </div>
   );
 }
