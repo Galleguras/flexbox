30052890ed987d4f79d422873a5c9dfc225f667c
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-01-03 13:12:21 +0100

    6 Propiedades en el contenedor Flex

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import './estilos.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className="container">
+      <div>PRIMERO</div>
+      <div>SEGUNDO</div>
+      <div>TERCERO</div>
     </div>
   );
 }
